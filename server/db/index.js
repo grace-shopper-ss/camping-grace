4cde6d4ba8063d3f72d253413ee97fdb5f0aed06
Spencer Posner <spencer.posner11@outlook.com>
2021-10-18 15:24:45 -0400

    ordered products model w associations

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,8 @@ const User = require("./models/User");
 const Product = require("./models/Product");
 const Inventory = require("./models/Inventory");
 const Order = require("./models/Order");
+const OrderedProduct = require("./models/OrderedProduct");
+
 //associations could go here!
 
 Product.hasMany(Inventory, { foreignKey: "productId" });
@@ -14,12 +16,18 @@ Inventory.belongsTo(Product, { foreignKey: "productId" });
 User.hasMany(Order, { foreignKey: "userId" });
 Order.belongsTo(User, { foreignKey: "userId" });
 
+Order.hasMany(OrderedProduct, { foreignKey: "orderId" });
+OrderedProduct.belongsTo(Order, { foreignKey: "orderId" });
+Inventory.hasOne(OrderedProduct, { foreignKey: "inventoryId" });
+
+
 module.exports = {
   db,
   models: {
     User,
     Product,
     Inventory,
-    Order
+    Order,
+    OrderedProduct,
   },
 };
