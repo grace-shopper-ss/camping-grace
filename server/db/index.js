5263dd38bc523e83b6f0d53d175e6cd032c5bf46
Spencer Posner <spencer.posner11@outlook.com>
2021-10-18 10:44:43 -0400

    added placeholder orders for users

--- server/db/index.js
+++ server/db/index.js
@@ -5,10 +5,14 @@ const db = require("./db");
 const User = require("./models/User");
 const Product = require("./models/Product");
 const Inventory = require("./models/Inventory");
+const Order = require("./models/Order");
 //associations could go here!
 
 Product.hasMany(Inventory, { foreignKey: "productId" });
 Inventory.belongsTo(Product, { foreignKey: "productId" });
+
+User.hasMany(Order, { foreignKey: "userId" });
+Order.belongsTo(User, { foreignKey: "userId" });
 
 module.exports = {
   db,
