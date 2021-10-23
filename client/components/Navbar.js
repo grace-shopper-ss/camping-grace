4dcf621321f7626014484794f51ed8b017063571
Spencer Posner <spencer.posner11@outlook.com>
2021-10-23 11:11:40 -0400

    added cart counter

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,13 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
-import theme from './Theme';
-import { ThemeProvider, Box, Toolbar, Typography, Button, IconButton, Menu, MenuItem, AppBar } from '@mui/material';
+import theme from "./Theme";
+import {
+  ThemeProvider,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  AppBar,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
 
-const Navbar = ({ isLoggedIn, handleLogout }) => {
+const Navbar = ({ isLoggedIn, handleLogout, cart }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -18,7 +28,7 @@ const Navbar = ({ isLoggedIn, handleLogout }) => {
     setAnchorEl(null);
   };
   return (
-    <ThemeProvider theme={theme}>  
+    <ThemeProvider theme={theme}>
       <div>
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static" id="nav" elevation={0}>
@@ -37,47 +47,64 @@ const Navbar = ({ isLoggedIn, handleLogout }) => {
               >
                 <MenuIcon />
               </IconButton>
-                {/* Start Menu */}
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                >
-                  <MenuItem className="menuText" onClick={handleClose}>
-                    <Link to={`/products`}>All Products</Link>
-                  </MenuItem>
-                  <MenuItem className="menuText" onClick={handleClose}>My account</MenuItem>
-                  <MenuItem className="menuText" onClick={handleLogout}>Logout</MenuItem>
-                </Menu>
-                {/* End Menu */}
-              <Typography id="main-heading" variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                <Link to={'/'}>
-                  Grace-Camping
-                </Link>
+              {/* Start Menu */}
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem className="menuText" onClick={handleClose}>
+                  <Link to={`/products`}>All Products</Link>
+                </MenuItem>
+                <MenuItem className="menuText" onClick={handleClose}>
+                  My account
+                </MenuItem>
+                <MenuItem className="menuText" onClick={handleLogout}>
+                  Logout
+                </MenuItem>
+              </Menu>
+              {/* End Menu */}
+              <Typography
+                id="main-heading"
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                <Link to={"/"}>Grace-Camping</Link>
               </Typography>
               <Link to="/cart" sx={{ flexGrow: 2 }}>
-                <IconButton size="large" aria-label="show 4 cart items" color="inherit">
-                  <Badge badgeContent={4} color="error">                    
+                <IconButton
+                  size="large"
+                  aria-label="show count of cart items"
+                  color="inherit"
+                >
+                  <Badge badgeContent={cart.length} color="error">
                     <ShoppingCartIcon />
                   </Badge>
                 </IconButton>
               </Link>
-              
+
               {isLoggedIn ? (
-                <Button onClick={handleLogout} color="inherit">Logout</Button>                
+                <Button onClick={handleLogout} color="inherit">
+                  Logout
+                </Button>
               ) : (
                 <div>
-                  <Link to="/signup"><Button color="inherit">Sign Up</Button></Link>
-                  <Link to="/login"><Button color="inherit">Login</Button></Link>
+                  <Link to="/signup">
+                    <Button color="inherit">Sign Up</Button>
+                  </Link>
+                  <Link to="/login">
+                    <Button color="inherit">Login</Button>
+                  </Link>
                 </div>
               )}
             </Toolbar>
           </AppBar>
-        </Box>                
+        </Box>
       </div>
     </ThemeProvider>
   );
@@ -88,6 +115,7 @@ const Navbar = ({ isLoggedIn, handleLogout }) => {
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
+    cart: state.cart,
   };
 };
 
